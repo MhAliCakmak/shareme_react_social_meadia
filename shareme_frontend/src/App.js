fffd373915cbc66428cded2a6710de0ad1186b64
mehmet5643 <202803055@ogr.cbu.edu.tr>
2023-03-15 11:41:38 +0300

    edited gitignore

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
-import { Routes, useNavigate, Route } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import {Login} from "./components"
 import Home from "./container/Home"
 import { GoogleOAuthProvider } from "@react-oauth/google";
 export default function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+    if (!User) navigate('/login');
+  }, []);
   return (
     <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_CLIENT_ID}`}>
     <Routes>
